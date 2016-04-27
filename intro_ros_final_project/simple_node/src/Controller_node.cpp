1fc06147b48a804ba256e53df9dd9a01306df5ae
Didac Coll <didaccoll_1993@hotmail.com>
2016-04-27 23:24:31 +0200

    newFeatures

--- intro_ros_final_project/simple_node/src/Controller_node.cpp
+++ intro_ros_final_project/simple_node/src/Controller_node.cpp
@@ -2,11 +2,13 @@
 #include "std_msgs/Empty.h"
 #include "std_srvs/Empty.h"
 #include "ar_pose/ARMarker.h"
+#include "geometry_msgs/Twist.h"
 
 #include <dynamic_reconfigure/server.h>
 #include <simple_node/dynparamsConfig.h>
 
 bool go_takeoff, go_land, change_cam;
+float xp, yp, zp, kp=2;
 
 void callback(simple_node::dynparamsConfig &config, uint32_t level) {
 
@@ -26,12 +28,12 @@ config.toggle_cam=false;
 
 void chatterCallback(const ar_pose::ARMarker msg)
 {
+  xp=msg.pose.pose.position.x;
+  yp=msg.pose.pose.position.y;
+  zp=msg.pose.pose.position.z;
 
   ROS_INFO("Pose: %f %f %f", //This is the marker floor position respect robot
-                            // cameracd
-	msg.pose.pose.position.x,
-  msg.pose.pose.position.y,
-  msg.pose.pose.position.z);
+                  xp,yp,zp); // cameracd
 }
 
 int main(int argc, char **argv)
@@ -42,7 +44,7 @@ int main(int argc, char **argv)
 
   ros::Publisher takeoff_pub = n.advertise<std_msgs::Empty>("ardrone/takeoff", 1000);
   ros::Publisher land_pub = n.advertise<std_msgs::Empty>("ardrone/land", 1000);
-
+  ros::Publisher velocity = n.advertise<geometry_msgs::Twist>("/cmd_vel",1000);
   dynamic_reconfigure::Server<simple_node::dynparamsConfig> server;
   dynamic_reconfigure::Server<simple_node::dynparamsConfig>::CallbackType f;
   f = boost::bind(&callback, _1, _2);
@@ -59,7 +61,7 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
     std_msgs::Empty msg;
-
+    geometry_msgs::Twist twist_msg; //Declare velocity variable
 //Take off the drone.
     if(go_takeoff)
     {
@@ -77,11 +79,16 @@ int main(int argc, char **argv)
     {
       camera.call(camera_srv);
       change_cam = false;
-
     }
-
+// Control position of mark1
+    twist_msg.linear.x=-kp*xp;
+    twist_msg.linear.y=-kp*yp;
+    velocity.publish(twist_msg);
+    ROS_INFO("Velocity: %f %f",-kp*xp,-kp*yp);
     ros::spinOnce();
     loop_rate.sleep();
+
+
   }
   return 0;
 }
